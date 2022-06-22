df8ba4d74cf912e3ce168f66f3dd4510b08716c1
Sazid AL Emon <sazid.emon@northsouth.edu>
2022-06-22 09:06:14 +0600

    complete the create ,read, update and delete part . It's Finish

--- Blog_App/urls.py
+++ Blog_App/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from . import views
+from Blog_App import views
 
 app_name = 'Blog_App'
 
 urlpatterns = [
     path('',views.BlogList.as_view(),name='Blog_List'),
     path('write/',views.CreateBlog.as_view(),name='Create_Blog'),
+    path('details/<slug>/',views.Blog_Details,name='Blog_Details'),
+    path('liked/<pk>/',views.liked, name='liked_post'),
+    path('unliked/<pk>/',views.unliked, name ='unliked_post'),
+    path('My-Blog',views.My_Blog.as_view(),name='My_Blog'),
+    path('Edit-Blog/<pk>',views.Edit_Blog.as_view(),name='Edit_Blog')
 ]
--- Blog_App/views.py
+++ Blog_App/views.py
@@ -4,7 +4,9 @@ from Blog_App.models import Blog,Comment,Likes
 from django.urls import reverse,reverse_lazy
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+from Blog_App.forms import CommentForm
 import uuid
+
 # Create your views here.
 
 
@@ -16,6 +18,16 @@ import uuid
 #def Blog_List(request):
 #    return render(request,'Blog_App/Blog_List.html',context={})
 
+class Edit_Blog(LoginRequiredMixin,UpdateView):
+    model = Blog
+    fields = ('blog_title','blog_content','blog_image')
+    template_name = 'Blog_App/Edit_Blog.html'
+
+    def get_success_url(self,**kwargs):
+        return reverse_lazy('Blog_App:Blog_Details',kwargs={'slug':self.object.slug})
+
+class My_Blog(LoginRequiredMixin, TemplateView):
+    template_name = 'Blog_App/My_Blog.html'
 
 class CreateBlog(LoginRequiredMixin,CreateView):
     model = Blog
@@ -33,3 +45,43 @@ class BlogList(ListView):
     context_object_name ='blogs'
     model = Blog
     template_name = 'Blog_App/Blog_List.html'
+    queryset = Blog.objects.order_by('-publish_date')
+
+@login_required
+def Blog_Details(request,slug):
+    blog = Blog.objects.get(slug=slug)
+    comment_form = CommentForm()
+    already_liked = Likes.objects.filter(blog=blog,user=request.user)
+    if already_liked:
+        liked=True
+    else:
+        liked = False
+    if request.method == 'POST':
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.user = request.user
+            comment.blog = blog
+            comment.save()
+            return HttpResponseRedirect(reverse('Blog_App:Blog_Details',kwargs={'slug':slug}))
+
+    return render(request,'Blog_App/Blog_Details.html',context={'blog':blog, 'comment_form':comment_form,'liked':liked})
+
+@login_required
+def liked(request,pk):
+    blog = Blog.objects.get(pk=pk)
+    user = request.user
+    already_liked = Likes.objects.filter(blog=blog,user=user)
+    if not already_liked:
+        liked_post =Likes(blog=blog,user=user)
+        liked_post.save()
+    return HttpResponseRedirect(reverse('Blog_App:Blog_Details',kwargs={'slug':blog.slug}))
+
+
+@login_required
+def unliked(request,pk):
+    blog = Blog.objects.get(pk=pk)
+    user = request.user
+    already_liked = Likes.objects.filter(blog=blog,user=user)
+    already_liked.delete()
+    return HttpResponseRedirect(reverse('Blog_App:Blog_Details',kwargs={'slug':blog.slug}))
